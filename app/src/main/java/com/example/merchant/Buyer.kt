d68906dd4ba7468c0c37021deff71c7c0d277ba0
Naphtali-cpu <naphtali919@gmail.com>
2021-10-06 22:12:47 +0300

    first commit

---
+++ app/src/main/java/com/example/merchant/Buyer.kt
@@ -0,0 +1,11 @@
+package com.example.merchant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Buyer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buyer)
+    }
+}
---
+++ app/src/main/java/com/example/merchant/ItemInStock.kt
@@ -0,0 +1,11 @@
+package com.example.merchant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ItemInStock : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_in_stock)
+    }
+}
