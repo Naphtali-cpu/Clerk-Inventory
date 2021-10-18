df2f02444f775d1bbf148cd5b0c01f235f6ae801
Naphtali-cpu <naphtali919@gmail.com>
2021-10-18 23:59:12 +0300

    Fixed add button

--- app/src/main/java/com/example/merchant/dashboardclass/ItemsReceived.kt
+++ app/src/main/java/com/example/merchant/dashboardclass/ItemsReceived.kt
@@ -3,33 +3,95 @@ package com.example.merchant.dashboardclass
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.merchant.R
+import com.example.merchant.adapters.MyAdapter
+import com.example.merchant.addition.AddItemsInStock
 import com.example.merchant.addition.AddItemsReceived
+import com.example.merchant.api.ApiInterface
+import com.example.merchant.models.MyDataItem
 import com.example.merchant.navigationbaractivities.Dashboard
+import kotlinx.android.synthetic.main.activity_item_in_stock.*
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ItemsReceived : AppCompatActivity() {
+    lateinit var myAdapter: MyAdapter
+    lateinit var linearLayoutManager: LinearLayoutManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_items_received)
 
-        val receive = findViewById(R.id.itemreceive) as ImageView
-        receive.setOnClickListener{
-            val intent = Intent(this, AddItemsReceived::class.java)
-            startActivity(intent)
-        }
+        recyclerViewItemInStock.setHasFixedSize(true)
+        linearLayoutManager = LinearLayoutManager(this)
+        recyclerViewItemInStock.layoutManager = linearLayoutManager
+        getMyData()
+
+//        val stock = findViewById(R.id.itemstock) as ImageView
+//        stock.setOnClickListener{
+//            val intent = Intent(this, AddItemsInStock::class.java)
+//            startActivity(intent)
+//        }
+
+//        val next = findViewById(R.id.c2) as CardView
+//        next.setOnClickListener{
+//            val intent = Intent(this, ItemDetails::class.java)
+//            startActivity(intent)
+//        }
+
 
         val main = findViewById(R.id.back) as ImageView
-//        val det = findViewById(R.id.item1) as CardView
 
         main.setOnClickListener{
             val intent = Intent(this, Dashboard::class.java)
             startActivity(intent)
         }
+    }
 
-//        det.setOnClickListener{
-//            val intent = Intent(this, ItemDetails::class.java)
-//            startActivity(intent)
-//        }
+    private fun getMyData() {
+        val okhttpHttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        val okHttpClient = OkHttpClient.Builder().addInterceptor(
+            okhttpHttpLoggingInterceptor
+        )
+
+        val retrofitBuilder = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .client(okHttpClient.build())
+            .build()
+            .create(ApiInterface::class.java)
+
+        val retrofitData = retrofitBuilder.getData("Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjM0NjI2MjI1LCJqdGkiOiJlZWFhYmRmMWI4YzU0OTJjYWMwYzM2YWJmNTRiOTVlZSIsInVzZXJfaWQiOjEyLCJyb2xlIjoiU1VQUExJRVIiLCJ1c2VybmFtZSI6Im5hcGh0YWxpOTE5QGdtYWlsLmNvbSJ9.pUmHhL4LaM8EPw6XKfn6bP3X83BwIUXBLtL5f-yzfMg").enqueue(object :
+            Callback<List<MyDataItem>> {
+            override fun onResponse(call: Call<List<MyDataItem>>, response: Response<List<MyDataItem>>) {
+                hideProgressBar()
+                if(response?.body() != null) {
+                    myAdapter = MyAdapter(baseContext, response.body()!!)
+                    recyclerViewItemInStock.adapter = myAdapter
+                    myAdapter.notifyDataSetChanged()
+                }
+//                val responseBody = response.body()
+            }
+
+            override fun onFailure(call: Call<List<MyDataItem>>, t: Throwable) {
+                hideProgressBar()
+                Log.d("ItemInStock", "onFailure:" + t.message)
+            }
+        })
+    }
+
+    private fun hideProgressBar() {
+        progressBar.setVisibility(View.GONE)
     }
 }
